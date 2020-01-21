b5a31c1dadf17441b52d82d1736f6281e44d7e1b
fortza_stiinta_craiova <fortza_stiinta_craiova@yahoo.com>
2020-01-21 21:56:55 +0200

    no message

--- src/Login/Login.component.ts
+++ src/Login/Login.component.ts
@@ -23,7 +23,7 @@ export class LoginComponent implements OnInit {
   constructor(private http: HttpClient,
     public authService: AuthService) {
       this.loginForm = new FormGroup({
-        email: new FormControl('', [Validators.required, Validators.email]),
+        email: new FormControl('', [Validators.required]),
         password: new FormControl('', [Validators.required])
       });
     }
